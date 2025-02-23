4d4c1598a4978922db8f2d7ca9456ece8ffae639
rogersrwr <160086701+rogersrwr@users.noreply.github.com>
2025-02-23 15:02:48 -0800

    json fix

--- global-setup.ts
+++ global-setup.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import * as fs from 'fs';
 import { json } from 'stream/consumers';
 
-const jsonData = require('C:/Users/ryanr/Desktop/stuff/brightarrow/automation/ParentHub Regs/datetime.json');
+const jsonData = require('D:/a/ParentHub-Regs/ParentHub-Regs/datetime.json');
 
 
 
